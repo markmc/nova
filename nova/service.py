4661ba0a5fb3964ef398aaac21f969b6b8c8b33f
Vishvananda Ishaya <vishvananda@gmail.com>
2011-11-05 14:10:17 -0700

    Separate metadata api into its own service
    part 1 of blueprint separate-metadata-api
    
     * adds api/metadata/ and moves code from ec2
     * moves metadata into separate binary
     * changes metadata forward to use metadata host and port
     * moves the metadata accept rule to the metadata api
     * adds 2011 to metadata versions
     * adds nova-api-* to setup.py
    
    Change-Id: I7f5d8e6cafc55b5c383cd88991f29c6059fb8d82

--- nova/service.py
+++ nova/service.py
@@ -45,7 +45,7 @@ flags.DEFINE_integer('report_interval', 10,
 flags.DEFINE_integer('periodic_interval', 60,
                      'seconds between running periodic tasks',
                      lower_bound=1)
-flags.DEFINE_string('ec2_manager', 'nova.api.manager.EC2Manager',
+flags.DEFINE_string('ec2_manager', None,
                     'EC2 API service manager')
 flags.DEFINE_string('ec2_listen', "0.0.0.0",
                     'IP address for EC2 API to listen')
@@ -55,6 +55,12 @@ flags.DEFINE_string('osapi_manager', None,
 flags.DEFINE_string('osapi_listen', "0.0.0.0",
                     'IP address for OpenStack API to listen')
 flags.DEFINE_integer('osapi_listen_port', 8774, 'port for os api to listen')
+flags.DEFINE_string('metadata_manager', 'nova.api.manager.MetadataManager',
+                    'OpenStack metadata service manager')
+flags.DEFINE_string('metadata_listen', "0.0.0.0",
+                    'IP address for metadata api to listen')
+flags.DEFINE_integer('metadata_listen_port', 8775,
+                     'port for metadata api to listen')
 flags.DEFINE_string('api_paste_config', "api-paste.ini",
                     'File name for the paste.deploy config for nova-api')
 
